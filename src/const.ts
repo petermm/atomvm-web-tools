144d92d8efe89068cbfee74c3f202cbad9ca9b94
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-07-11 13:42:17 +0200

    add P4

--- src/const.ts
+++ src/const.ts
@@ -13,7 +13,8 @@ export interface Build {
     | "ESP32-C2"
     | "ESP32-C3"
     | "ESP32-C6"
-    | "ESP32-H2";
+    | "ESP32-H2"
+    | "ESP32-P4";
   parts: {
     path: string;
     offset: number;
